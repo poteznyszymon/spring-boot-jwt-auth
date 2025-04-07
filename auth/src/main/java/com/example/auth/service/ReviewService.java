781afa833551bb7177e3683a4247549a773fc5aa
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2025-04-07 17:09:40 +0200

    Added logic to delete images from cloudinary when user delete review

--- auth/src/main/java/com/example/auth/service/ReviewService.java
+++ auth/src/main/java/com/example/auth/service/ReviewService.java
@@ -91,8 +91,6 @@ public class ReviewService {
         }
         restaurant.setTotalReviews(restaurant.getTotalReviews() - 1);
 
-
-
         for (ImageEntity image: review.getImages()) {
             cloudinaryService.deleteImageByUrl(image.getUrl());
         }
